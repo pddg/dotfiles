3069f9d8f017f1bb3123b7fad18c2ed7c20111dd
Pudding <s.kokuryo@gmail.com>
2019-06-26 20:56:24 +0900

    Update config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,14 @@
 # Env vars
 set -x GOPATH $HOME/go
-set -x APP_ENGINE_SDK_PATH /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/platform/google_appengine
+if test -d $GOPATH
+    set -x PATH $GOPATH/bin $PATH
+end
+# Poetry
 set -x POETRY_ROOT $HOME/.poetry
 if test -d $POETRY_ROOT
     set -x PATH $POETRY_ROOT/bin $PATH
 end
-set -x PATH $GOPATH/bin $APP_ENGINE_SDK_PATH $PATH
+
 set -x LANG en_US.UTF-8
 set -x LC_ALL en_US.UTF-8
 set -x XDG_CONFIG_HOME $HOME/.config
@@ -25,18 +28,27 @@ set -x theme_display_user no
 set -x theme_date_format "+%m/%d %H:%M(%a)"
 set -x theme_display_virtualenv yes
 set -x theme_powerline_fonts yes
+set -x theme_git_worktree_support no
 set -x theme_display_git_ahead_verbose yes
-set -x theme_git_worktree_support yes
-set -g theme_display_git_master_branch yes
+set -x theme_display_git_master_branch yes
 set -x theme_color_scheme dark
-set -g fish_prompt_pwd_dir_length 1
-set -g theme_project_dir_length 1
-set -g theme_display_cmd_duration no
-set -g theme_display_k8s_context no
+set -x fish_prompt_pwd_dir_length 1
+set -x theme_project_dir_length 1
+set -x theme_display_cmd_duration no
+set -x theme_display_k8s_context no
+
+# Setup hilighting
+set -x fish_color_error ff260d
+set -x fish_color_command 00a1f9
+set -x fish_color_param 00ddef
+set -x fish_color_quote ffc400
+set -x fish_color_redirection ab7aef
+set -x fish_color_escape 9ae204
+set -x fish_color_operator 9ae204
 
 if type nvim > /dev/null 2>&1
     # alias
-    balias vim 'nvim'
+	balias vim "nvim"
 end
 
 if type anyenv > /dev/null 2>&1
